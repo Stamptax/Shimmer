640785a132b3447bac8be1252e48f025a2047e1b
Stamptax <>
2024-04-11 00:31:59 +0930

    animations added

--- src/pages/Recruit/Recruit.js
+++ src/pages/Recruit/Recruit.js
@@ -1,10 +1,37 @@
 import './Recruit.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from 'react';
+
 
 export default function Recruit({ hiringText, subHiringText, jobs, contactInfo, contactApp }) {
     const ToContact = (e) => {
         e.preventDefault();
         document.getElementById("footer-id").scrollIntoView({ behavior: 'smooth' });
     };
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+        const recruitElement = document.querySelector('.recruit');
+        if (recruitElement) {
+            const width = recruitElement.offsetWidth;
+            gsap.set(recruitElement, { x: -width });
+        }
+        gsap.to(
+            '.recruit', {
+            x: 0,
+            scrollTrigger: {
+                trigger: '.recruit',
+                start: 'top 90%',
+                end: 'top center',
+                scrub: true,
+            }
+        }
+        )
+    }
+    )
+
+
 
     return (
         <div className='recruit' id='hiring-board'>
